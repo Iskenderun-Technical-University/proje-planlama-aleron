dbaafc2a9fa671ac6aabc7b16d49aca856c315a0
Erol GÖNÜL <92864814+beerolum@users.noreply.github.com>
2022-05-11 17:50:46 +0300

    Create ana.php
    Okul Dijital Pano Admin Paneli

---
+++ admin/ana.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+        <title>Ramazanoğlu MTAL - Dijital Pano</title>
+        <link rel="shortcut icon" href="images/terminal.ico">
+
+        <!-- C3 Chart css -->
+        <link href="assets/libs/c3/c3.min.css" rel="stylesheet" type="text/css" />
+
+        <!-- App css -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bootstrap-stylesheet" />
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/app.min.css" rel="stylesheet" type="text/css"  id="app-stylesheet" />
+</head>
+
+<body>
+	<center>
+		<h1>RAMAZANOĞLU MTAL DİJİTAL PANO</h1>
+		<h2>YÖNETİM PANELİNE</h2>
+		<img src="images/hos.gif" width="30%" height="30%">
+	</center>
+</body>
+</html>
